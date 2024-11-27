672d0473acca5178364e95090d0ae0ffcba854a5
Donatas Petrauskas <donatasp@wix.com>
2024-11-27 15:13:28 +0200

    fix set_env_local

--- bin/set_env_local.sh
+++ bin/set_env_local.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Initialize frontend monorope with .env.local files with my preferred
+# Initialize frontend monorepo with .env.local files with my preferred
 # configuration.
 
-ENVS=$(find . -type d -name node_modules -prune -o -name .env -print)
-
-echo $ENVS | while read -d ' ' ENV; do
-    echo Creating ${ENV}.local ...
-    echo TRUSED_CERTIFICATE=false >> ${ENV}.local
-    echo PROGRESS_BAR=false >> ${ENV}.local
-done
+find . -type d -name node_modules -prune -o -name .env -print |
+    while read ENV; do
+        echo Creating ${ENV}.local ...
+        echo TRUSED_CERTIFICATE=false >> ${ENV}.local
+        echo PROGRESS_BAR=false >> ${ENV}.local
+    done
